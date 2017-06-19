7c7fdbbf26de9b20edb8ea4484edb4f5641ae2be
tancy <ronntx@gmail.com>
2017-06-19 18:07:35 +0800

    * [doc] update delploy.sh fatch code

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -5,7 +5,10 @@ WEB_PATH='/var/www/html/weex'
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git checkout gh-pages
-git pull origin gh-pages
-git checkout gh-pages
+
+git fetch --all
+git reset --hard origin/gh-pages
+# git checkout gh-pages
+# git pull origin gh-pages
+# git checkout gh-pages
 echo "Finished."
